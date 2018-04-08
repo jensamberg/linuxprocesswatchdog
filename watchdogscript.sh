50567ba7b5311e9937e28996dacba871c7cf8f26
Jens Amberg <jensamberg@mail.de>
2018-04-09 01:03:10 +0800

    fix if condition

--- watchdogscript.sh
+++ watchdogscript.sh
@@ -3,7 +3,7 @@
 # in the next line replace processname with your process
 var=`ps -ea | grep processname | grep -v grep | wc -l`
 
-if [ "$var" -lt "1" ]; then
+if [ "$var" = "1" ]; then
      echo "process is running"
 else
      echo "process is not running"
